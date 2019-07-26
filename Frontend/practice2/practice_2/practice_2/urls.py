29a14845445f87ea1377800793233196cfec77e8
aghebert <30908336+aghebert@users.noreply.github.com>
2019-07-26 16:27:39 -0400

    Weeky Status Report Week 1 - Aaron Done

--- Frontend/practice2/practice_2/practice_2/urls.py
+++ Frontend/practice2/practice_2/practice_2/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('show_DB/', include('show_DB.urls')),
     path('admin/', admin.site.urls),
